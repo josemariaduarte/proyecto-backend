22ea82573f987697acdc822991f22ff123160fbe
Jose Duarte <josemduarte91@gmail.com>
2021-08-24 21:06:53 -0400

    agregamos al admin

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from productos.models import Categoria
+
+@admin.register(Categoria)
+class CategoriaoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nombre',  'activo']
