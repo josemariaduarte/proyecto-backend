acfcce98efa3b4a7fb2c9974e093e4be2f9d4220
Jose Duarte <josemduarte91@gmail.com>
2021-08-26 21:33:19 -0400

    update admin personas

--- personas/admin.py
+++ personas/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from personas.models import Cliente, Proveedor
+
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['nombres', 'apellidos', 'activo']
+
+
+@admin.register(Proveedor)
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ['razon_social', 'ruc', 'activo']
