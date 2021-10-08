10957a9a70572edf55787aaedfc585c73d725822
Jose Duarte <josemduarte91@gmail.com>
2021-10-08 12:01:13 -0300

    edit compra y and compra detail, update product cantidad and precio

--- movimientos/serializers.py
+++ movimientos/serializers.py
@@ -1,8 +1,10 @@
 from django.db import transaction
 from django.utils import timezone
 from rest_framework import serializers
+from rest_framework.utils import model_meta
 
 from movimientos.models import OrdenCompra, OrdenCompraDetalle, CompraDetalle, Compra
+from utils.functions import list_diff
 from utils.serializers import BaseModelSerializer
 
 class OrdenCompraDetalleSerializer(BaseModelSerializer):
@@ -106,6 +108,62 @@ class CompraSerializer(BaseModelSerializer):
         #
         return compra
 
+    @transaction.atomic
+    def update(self, compra, validated_data):
+        k = 'compradetalle_set'
+        if k in validated_data:
+            del validated_data[k]
+        compra = super(CompraSerializer, self).update(compra, validated_data)
+        # actualizacion de dias
+        detalles = self.initial_data.get('detalles', [])
+        self.update_detalles(compra, detalles)
+        # actualizamos los montos total
+        monto = 0
+        for detalle in detalles:
+            monto += (float(detalle['cantidad']) * float(detalle['precio']))
+        #
+        compra.total = monto
+        compra.save()
+        return compra
+
+    def update_detalles(self, compra, detalles):
+        queryset = compra.compradetalle_set
+        actuales = queryset.all()
+        nuevos = [a for a in detalles if 'id' not in a.keys()]
+        # Los elementos que esten en actuales y no esten en detalles son eliminados
+        eliminados = list_diff([a['id'] for a in actuales.values('id')],
+                               [a['id'] for a in detalles if 'id' in a.keys()])
+
+        for detalle in detalles:
+            if 'id' in detalle.keys():
+                changed_fields = []
+                obj = CompraDetalle.objects.get(pk=detalle['id'])
+                info = model_meta.get_field_info(obj)
+                for attr, value in detalle.items():
+                    if attr in info.relations.keys():
+                        attr += "_id"
+                    if getattr(obj, attr) != value:
+                        changed_fields.append(attr)
+                    # actualizamos la cantidad del producto
+                    producto = obj.producto
+                    if attr == 'cantidad':
+                        producto.cantidad = producto.cantidad + float(value) - obj.cantidad
+                        producto.save()
+                    elif attr == 'precio':
+                        # solo si se modifico el precio
+                        if producto.precio_compra != float(value):
+                            producto.precio_compra = float(value)
+                            producto.precio_venta += float(value) * (producto.porcentaje_ganancia / 100)
+                            producto.fecha_modificacion_precio_venta = timezone.now()
+                            producto.save()
+                    setattr(obj, attr, value)
+                obj.save()
+        #
+        for data in nuevos:
+            CompraDetalle.objects.create(compra=compra, **data)
+        #
+        for obj in queryset.filter(pk__in=eliminados):
+            obj.delete()
 
 
 
